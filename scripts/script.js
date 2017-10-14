7291eeb2ef02727c7e86126784eb91365f929a82
kozulenko_d <geograff86@gmail.com>
2017-10-14 23:39:07 +0300

    opacity

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,9 @@
-// var close = document.querySelector('.cross-button');
-// close.onclick = function() {
-//     document.getElementsByTagName('nav')[0].style.display = 'none';
-// }
 window.onload = function() {
 	var cross = document.querySelector('.cross-button');
 	var burg = document.querySelector('.burger-button');
 	var nav = document.querySelector('nav');
 	var lis = nav.querySelector('ul').querySelectorAll('li');
 
-	// if (document.body.clientWidth >= 768 && document.body.clientWidth <= 1023) {
-	// 	burg.onclick = function () {
-	// 		burg.classList.toggle('hidden');
-	// 		cross.classList.toggle('hidden');
-	// 		document.querySelector('.logo-box').querySelector('span').classList.toggle('hidden');
-	// 		document.querySelector('nav').querySelector('ul').classList.toggle('hidden');
-	// 		document.querySelector('nav').style.height = '135px';
-	// 	}
-	// 	cross.onclick = function() {
-	// 		burg.classList.toggle('hidden');
-	// 		cross.classList.toggle('hidden');
-	// 		document.querySelector('.logo-box').querySelector('span').classList.toggle('hidden');
-	// 		document.querySelector('nav').querySelector('ul').classList.toggle('hidden');
-	// 		document.querySelector('nav').style.height = '64px';
-	// 	}
-	// }
 	if (document.body.clientWidth >= 481 && document.body.clientWidth <= 767) {
 		burg.onclick = function() {
 			burg.style.display = 'none';
@@ -67,4 +47,3 @@ window.onload = function() {
 		}
 	}
 }
-
